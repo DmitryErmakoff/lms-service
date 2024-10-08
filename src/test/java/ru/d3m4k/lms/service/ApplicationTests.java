34e8738e6308d906f50eeefca6787a21464ac7e0
DmitryErmakoff <dimka6466erm@gmail.com>
2024-10-08 19:33:48 +0500

    init project

---
+++ src/test/java/ru/d3m4k/lms/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.d3m4k.lms.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
